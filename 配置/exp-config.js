09dfe05c02323f7b18c968c5201003fed8c4a0fb
ybtxyg <111720544+ybtxyg@users.noreply.github.com>
2022-12-28 19:02:06 +0800

    Update and rename config/exp-config.js to 配置/exp-config.js

--- 配置/exp-config.js
+++ 配置/exp-config.js
@@ -2,3 +2,4 @@ import USER_CONFIG from './index.cjs'
 
 const config = process.env.USER_INFO ? /* istanbul ignore next */ JSON.parse(process.env.USER_INFO) : USER_CONFIG
 export default config
+jhgvuy
